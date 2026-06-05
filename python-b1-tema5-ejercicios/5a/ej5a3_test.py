eeb90d2abe1b1a34423c452e64d1a90bb439087b
ANTONIO CASTEL MILLÁN <casantphys@hotmail.com>
2026-06-05 20:14:49 +0200

    Eliminar carpetas incorrectas

--- python-b1-tema5-ejercicios/5a/ej5a3_test.py
+++
@@ -1,9 +0,0 @@
-from ej5a3 import SafeWalletCredentials
-
-
-def test_wallet():
-    wallet = SafeWalletCredentials("1234")
-    assert wallet.get_password() == "1234", "Incorrect password retrieval"
-
-    wallet.set_password("4321")
-    assert wallet.get_password() == "4321", "Incorrect password setting"
