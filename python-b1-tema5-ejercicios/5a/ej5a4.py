bfaea6b59579fa7a4d4ebf9ae6aa4e8848f717bc
ANTONIO CASTEL MILLÁN <casantphys@hotmail.com>
2026-06-05 18:49:14 +0200

    Add files via upload

--- python-b1-tema5-ejercicios/5a/ej5a4.py
+++ python-b1-tema5-ejercicios/5a/ej5a4.py
@@ -95,15 +95,30 @@ Exemple:
 """
 
 # Write class DatabaseConnector here
-
+class DatabaseConnector:
+    def connect(self, database):
+        database.connected = True
 # Write class SQLDatabase here
-
-
+class SQLDatabase:
+    def __init__(self):
+        self.connected = False
 # Write class PostgresDatabase here
-
-
+class PostgresDatabase:
+    def __init__(self):
+        self.connected = False
 # Write class RedshiftDatabase here
-
+class RedshiftDatabase:
+    def __init__(self):
+        self.connected = False
+db_connector = DatabaseConnector()
+sql_db = SQLDatabase()
+postgres_db = PostgresDatabase()
+redshift_db = RedshiftDatabase()
+print(sql_db.connected, postgres_db.connected, redshift_db.connected)
+db_connector.connect(sql_db)
+db_connector.connect(postgres_db)
+db_connector.connect(redshift_db)
+print(sql_db.connected, postgres_db.connected, redshift_db.connected)
 
 # Por la propia naturaleza del ejercicios no se puede probar imprimiendo por pantalla, 
 # revisa los tests para revisar la corrección de tu propuesta de solución
